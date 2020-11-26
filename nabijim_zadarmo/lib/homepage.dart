a1635713ea18041e1e95a29cb089ded6b4e80a18
David Vávra <david@vavra.me>
2020-11-26 21:37:58 +0100

    Launcher working & simplify

---
+++ nabijim_zadarmo/lib/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nabíjím zdarma"),
+      ),
+      body: Center(
+      )
+    );
+  }
+
+}
